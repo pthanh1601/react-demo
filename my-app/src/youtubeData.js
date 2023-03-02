14b4cc6cee37c53f60421998d25ece7cc88ecc8a
thanh trinh <trinhthanh160102@gmail.com>
2023-03-02 22:22:23 +0700

    update sourcecode

---
+++ my-app/src/youtubeData.js
@@ -0,0 +1,26 @@
+export const youtubeData = [
+  {
+    id: 1,
+    title: "no title1",
+    author: "no author1",
+    Image: "https://unsplash.com/fr/photos/wEL2zPX3jDg",
+    avatar:
+      "https://lh3.googleusercontent.com/a/AGNmyxbJ9t0rBDDRjwETmpVsNGgjoYKRAtw_DVAi9EiL=s360",
+  },
+  {
+    id: 2,
+    title: "no title2",
+    author: "no author2",
+    image: "https://unsplash.com/fr/photos/wEL2zPX3jDg",
+    avatar:
+      "https://lh3.googleusercontent.com/a/AGNmyxbJ9t0rBDDRjwETmpVsNGgjoYKRAtw_DVAi9EiL=s360",
+  },
+  {
+    id: 3,
+    title: "no title3",
+    author: "no author3",
+    Image: "https://unsplash.com/fr/photos/wEL2zPX3jDg",
+    avatar:
+      "https://lh3.googleusercontent.com/a/AGNmyxbJ9t0rBDDRjwETmpVsNGgjoYKRAtw_DVAi9EiL=s360",
+  },
+];
